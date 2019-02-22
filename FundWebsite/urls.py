baeab6dcd4b7d22adaf551980d8ee7dfb2c8868d
XiangLi <xiangli.lu@outlook.com>
2019-02-22 13:57:06 +0800

    datepicker complete

--- FundWebsite/urls.py
+++ FundWebsite/urls.py
@@ -6,6 +6,6 @@ from fundapp.views import home, mds, test
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home),
-    url(r'^test/(\w+)/$', test),
-    url(r'^ajax_list/(\w+)/$', mds),
+    url(r'^test/(?P<start>\d{4}-\d{2}-\d{2}) (?P<end>\d{4}-\d{2}-\d{2})/$', test),
+    url(r'^ajax_list/(?P<start>\d{4}-\d{2}-\d{2}) (?P<time>\d{4}-\d{2}-\d{2})/$', mds),
 ]
--- static/javascript/test.js
+++ static/javascript/test.js
@@ -1,11 +1,19 @@
 $(function() {
   pre_time = ''
   $("#profit").mousemove(function() {
+    start = document.URL;
+    start = start.split('/')[4]
+    start = start.split('%')[0]
+    start = start.split('-')
+    start = start[0] + '-' + start[1] + '-01'
+
     time = $("#profit, .bk-canvas-overlays")
     time = time.children().find('span').html()
-    time = time.split('-')[1]
+    time = time.split('-')
+    time = time[0] + '-' + time[1] + '-01'
+    start = start + " " + time
     if (pre_time != time) {
-      $.getJSON('/ajax_list/' + time, function(ret) {
+      $.getJSON('/ajax_list/' + start, function(ret) {
         $("#mds").html(ret.div + ret.script);
       })
       pre_time = time;

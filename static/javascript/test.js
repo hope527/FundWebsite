44cdeccaa490e7b60a34f7834c7025954b858539
hope527 <32390556+hope527@users.noreply.github.com>
2019-11-14 02:53:42 +0800

    Add files via upload

--- static/javascript/test.js
+++ static/javascript/test.js
@@ -1,3 +1,23 @@
 $(function () {
     $("#form").validate();
+
+    $("#comprehensive").click(function () {
+        $("#risk_value").hide();
+        $("#comprehensive_value").show();
+    })
+
+    $("#risk").click(function () {
+        $("#comprehensive_value").hide();
+        $("#risk_value").show();
+    })
+
+    $("#single").click(function () {
+        $("#strategy2").hide();
+        $("#strategy1").show();
+    })
+
+    $("#regular").click(function () {
+        $("#strategy1").hide();
+        $("#strategy2").show();
+    })
 });
--- static/javascript/test_show.js
+++ static/javascript/test_show.js
@@ -5,16 +5,20 @@ $(function () {
     $(".loading").remove();
     response_data = ret;
     $("#sharpe_ratio").text("夏普指標: " + response_data.sharpe_ratio);
+    $("#market_sharpe").text("市場夏普指標: " + response_data.market_sharpe);
     $("#std").text("標準差: " + response_data.std);
+    $("#market_std").text("市場標準差: " + response_data.market_std);
     $("#beta").text("beta: " + response_data.beta);
     $("#treynor_ratio").text("Treynor指數: " + response_data.treynor_ratio);
     $("#money").text("期末金額: " + response_data.money);
     $("#profit").text("期末報酬率(%): " + response_data.profit + "%");
-    $("#simulation").text("預估五年後報酬率(%): " + response_data.simulation + "%");
+    $("#market_revenue").text("市場期末報酬率(%): " + response_data.market_revenue + "%");
+    // $("#simulation").text("預估五年後報酬率(%): " + response_data.simulation + "%");
     $("#mean_similarity").text("平均距離: " + response_data.mean_similarity);
     $("#profit_img").html(response_data['profit_img'].div + response_data['profit_img'].script);
-    $("#predict_price").html(response_data['predict_price'].div + response_data['predict_price'].script);
+    $("#distance").html(response_data['distance'].div + response_data['distance'].script);
     $("#mds").html(response_data[response_data["start"]].div + response_data[response_data["start"]].script);
+    $("#table").show()
   })
 
   var temp = ''

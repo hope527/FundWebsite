0c5c2a2779a7a98a7dc15f85d26870a4c89dde4d
hope527 <32390556+hope527@users.noreply.github.com>
2019-11-14 02:52:48 +0800

    Add files via upload

--- FundWebsite/urls.py
+++ FundWebsite/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from fundapp.views import index, index_response, test, test_respoonse, search, index_form
+from fundapp.views import index, index_response, test, test_respoonse, search, index_form, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('index/c=<str:column>&key=<str:keyword>', search),
     path('index/id=<str:fund_id>&area=<str:area>', index_form),
     path('test/', test),
+    path("contact/",contact),
     path('test/<slug:start>&<slug:end>&<str:investement_type>&<str:ratio>&<int:btest_time>&<int:money>&<str:buy_ratio>&<int:strategy>&<int:frequency>/', test_respoonse),
 ]
